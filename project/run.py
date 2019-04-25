9c80f5cd702aa52f7c81860296faa8290d88339f
Mauricio Hernández <mauricio.hernandez2@correo.icesi.edu.co>
2019-04-24 19:49:18 -0500

    Suban las cosas

--- project/run.py
+++ project/run.py
@@ -1,9 +1,16 @@
 """
-Es el programa, encargado de usar
-la controladora y la consola para
+Es el programa, encargado de usarla controladora y la consola para
 interactuar con el usuario.
+
+
 
 @Author Santiago Gutierrez
 @Author Mauricio Hernandez
 
 """
+import sys
+
+def parameterType():
+    if(sys.argv[1] == "-t"):
+        print("Training")
+parameterType()
