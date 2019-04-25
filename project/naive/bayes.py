751fc87a5b9ed138e31263ef7466a3f4da4df4f3
unknown <alejandrobravo0329@gmail.com>
2019-04-24 23:02:00 -0500

    constructors

--- project/naive/bayes.py
+++ project/naive/bayes.py
@@ -1,76 +1,71 @@
-from numpy import zeros
 from numpy import *
-class MachingLearn:
+
+class MachineLearning:
     
         # private Dictionary<int,Category> categories;
         # private int[][] dataInputTraining;
         # private int[] dataOutputTraining;
 
 
-        def __init__():
+    def __init__(self):
         
-            categories = {}
+        self.categories = {}
+        self.dataInputTraining=[]
+        self.dataOutputTraining=[]
         
         
-        def Decide(self,input,ret):
+    def Decide(self,input,ret):    
+        res = True
+        ret =zeros(input.Length)
+        for i in range(input.Length):    
+             res = res and DecideV(input[i],ret[i])
+        return res
         
-            res = True
-            ret =zeros(input.Length)
-            for i in range(input.Length):
-            
-                res = res and DecideV(input[i],ret[i])
-               
-            
-            return res
-        
-        def DecideV(self,input, ret):
-            ret = -1
-            pro = Double.MinValue
-            for  a in self.keys:
-                tmp=self.categories[a]
-                prop = tmp.Propiedades
-                plus = tmp.Plus
-                sum = tmp.Proba
-                for  i in range(prop):
+    def DecideV(self,input, ret):
+        ret = -1
+        pro = Double.MinValue
+        for  a in self.keys:
+            tmp=self.categories[a]
+            prop = tmp.Propiedades
+            plus = tmp.Plus
+            sum = tmp.Proba
+            for  i in range(prop):
                     
-                   if (input[i] != 0):
+               if (input[i] != 0):
                   
-                       sum *= plus[i] * input[i]
+                   sum *= plus[i] * input[i]
                                    
-                if(sum > pro ): ret=a 
-                if(sum > pro): pro=sum 
+            if(sum > pro ): ret=a 
+            if(sum > pro): pro=sum 
                 
-            return ret
+        return ret
         
-        def Learn(self,inputL,outputL):
+    def Learn(self,inputL,outputL):
+        moment = {}
+        for i in range(input.Length):      
+            tmp = inputL[i]
+            clas = outputL[i]
+            lis= moment.get(clas)
+            nExist=lis==None
+            if (nExist):                
+                lis = {}                
+            lis.append(tmp)
+            if (nExist):
+                moment.setdefault(clas,lis)                      
+        keys = moment.Keys()
+        for  a in keys:
+            lis=moment.get(a)
+            if (lis!=None):
+                ca = Category(a,(len(lis)/ len(inputL),lis)
+                # self.categories.setdefault(a,ca)
+                            
+        # self.dataInputTraining = inputL
+        # self.dataOutputTraining = outputL
         
-            moment = {}
-            for i in range(input.Length):
-            
-                tmp = inputL[i]
-                clas = outputL[i]
-                lis= moment.get(clas)
-                nExist=lis==None
-                if (nExist):                
-                  lis = {}                
-                lis.append(tmp)
-                if (nExist):
-                    moment.setdefault(clas,lis)              
-            
-            keys = moment.Keys()
-            for  a in keys:
-                lis=moment.get(a)
-                if (lis!=None):
-                    ca = new Category(a,(len(lis)/ len(inputL),lis)
-                    categories.Add(a, ca);
-            
-            self.dataInputTraining = inputL
-            self.dataOutputTraining = outputL
-
-
 
 class Category:
-    def Category(self, name, pr, values):
+
+    def __init__(self, name, pr, values):
         self.Proba = pr
         self.name = name
         self.values = values
